7084d15f24ae64a1ef1352ec91682c65c1f6c43d
Enrico Badan Orsatti <84781406+EnricoOrsatti@users.noreply.github.com>
2024-05-10 17:34:40 +0100

    (10/05). Today I started first with reading the CSV and the calculator. I couldnt finish any.

--- src/Classes/BasalMetabolism.kt
+++ src/Classes/BasalMetabolism.kt
@@ -1,4 +1,16 @@
 package Classes
+import java.io.File
 
-class BasalMetabolism {
+
+class BasalMetabolism (wheight: Double, height: Double, age: Int) {
+    val W = wheight
+    val H = height
+    val A = age
+
+    fun Calc(): Double {
+
+        val Calculator = 88.362 + (13.397 * W) + (4.799 * H) - (5.677 * A)
+
+        return Calculator
+    }
 }
--- src/Classes/ReadCSV.kt
+++ src/Classes/ReadCSV.kt
@@ -1,4 +1,15 @@
 package Classes
+import  java.io.File
 
-class ReadCSV {
+class ReadCSV(CSVHere: File) {
+        fun CSVreader(){
+            val filePath = "CSVHere"
+            val file = File(filePath)
+
+        file.forEachLine { line ->
+            val tokens = line.split(",")
+
+            println(tokens)
+        }
+    }
 }
