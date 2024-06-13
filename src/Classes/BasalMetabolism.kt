ddda592f068de2e68b14ef7115955b1f07fbd990
Enrico Badan Orsatti <84781406+EnricoOrsatti@users.noreply.github.com>
2024-06-13 12:50:03 +0100

    H2 database is now conected with the project, I think!

--- src/Classes/BasalMetabolism.kt
+++ src/Classes/BasalMetabolism.kt
@@ -2,15 +2,21 @@ package Classes
 import java.io.File
 
 
-class BasalMetabolism (wheight: Double, height: Double, age: Int) {
+open class BasalMetabolism (wheight: Double, height: Double, age: Int) {
     val W = wheight
     val H = height
     val A = age
+
 
     fun Calc(): Double {
 
         val Calculator = 88.362 + (13.397 * W) + (4.799 * H) - (5.677 * A)
 
-        return Calculator
+        return Calculator;
+
+        var Kcal = Double;
+        var Objetivo = String;
+
+        if (Kcal >= 2500.00, Objetivo = "Quero perder peso") {}
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,13 @@
-import Classes.ReadCSV
-import java.io.File
+package com.example
 
+import org.jetbrains.exposed.sql.Database
 
 fun main() {
-    println("To Fudido!")
-    ReadCSV
+    val database =
+        Database.connect(
+                    url = "jdbc:h2:file:./database/db;MODE=MYSQL;DB_CLOSE_DELAY=-1;",
+                    user = "root",
+                    driver = "org.h2.Driver",
+                    password = ""
+            )
 }
